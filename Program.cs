b81697bb282058d907fa1f3271570a87730378ad
Alex <aleksejtatukov098@gmail.com>
2023-08-11 09:43:59 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿//КОММИТЫ НЕ ЗАБЫТЬ
-int clone = 0;
-string [] Array1 = {"Snake", "32", "error", "qwerty", "65"};
-string [] Array2 = {Array1.Length};
+
+string [] Array1 = {"Ske", "32", "error", "qwerty", "65"};
+string [] Array2 = new string[Array1.Length];
+void first (string [] Array1, string [] Array2)
+{
+  int clone = 0;
 for (int i = 0; i < Array1.Length; i++)
 {
   if (Array1[i].Length <= 3 )
@@ -10,3 +13,15 @@ Array2[clone] = Array1[i];
 clone++;
   }
 }
+}
+void second (string [] Array2)
+{
+{
+for (int i = 0; i < Array1.Length; i++)
+{
+Console.Write( Array2[i]+ " ");
+}
+}
+}
+first(Array1, Array2);
+second (Array2);
