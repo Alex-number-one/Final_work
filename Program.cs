5c9a4867ecc2c4b5df5e046e6c4c852c26505067
Alex <aleksejtatukov098@gmail.com>
2023-08-11 09:15:40 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//КОММИТЫ НЕ ЗАБЫТЬ
+int clone = 0;
+string [] Array1 = {"Snake", "32", "error", "qwerty", "65"};
+string [] Array2 = {Array1.Length};
+for (int i = 0; i < Array1.Length; i++)
+{
+  if (Array1[i].Length <= 3 )
+  {
+Array2[clone] = Array1[i];
+clone++;
+  }
+}
